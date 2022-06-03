f6974544a52a945e64f7bb436bb2841baaabc867
senka <senkamilutinovic@gmail.com>
2022-06-03 15:44:16 +0200

    fixed pop up, added instructions

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const spaces = JSON.parse(fs.readFileSync('spaces.json'));
 
 app.use(express.static('public'));
